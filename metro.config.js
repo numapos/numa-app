281a75744b06c43dc859ae3e7a61b0002f1d2ece
SalsaCat <laugessvan@gmail.com>
2026-07-25 21:17:10 +0200

    fix: metro config svg resolver

--- metro.config.js
+++ metro.config.js
@@ -10,6 +10,9 @@ config.resolver.assetExts = config.resolver.assetExts.filter(
   (ext) => ext !== "svg"
 );
 
-config.resolver.sourceExts.push("svg");
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
+  "svg",
+];
 
 module.exports = config;
